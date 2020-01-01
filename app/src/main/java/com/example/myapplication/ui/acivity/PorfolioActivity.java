b40672c4739338c1031318eddc1817d6fb59e37c
XiaoHuisir <860663594@qq.com>
2020-01-01 23:43:43 +0800

    实现我的模块中学习记录功能修改01

--- app/src/main/java/com/example/myapplication/ui/acivity/PorfolioActivity.java
+++ app/src/main/java/com/example/myapplication/ui/acivity/PorfolioActivity.java
@@ -76,17 +76,17 @@ public class PorfolioActivity extends BaseActivity {
                 break;
             case R.id.btn_types_all:
                 Constant.CURTYPE = Constant.STUD_TYPE_0;
-                Toast.makeText(context,"1111111",Toast.LENGTH_LONG).show();
+                Toast.makeText(context,"1",Toast.LENGTH_LONG).show();
                 allTypes();
                 break;
             case R.id.btn_types_train:
                 Constant.CURTYPE = Constant.STUD_TYPE_1;
-                Toast.makeText(context,"2222",Toast.LENGTH_LONG).show();
+                Toast.makeText(context,"2",Toast.LENGTH_LONG).show();
                 trainTypes();
                 break;
             case R.id.btn_types_lesson:
                 Constant.CURTYPE = Constant.STUD_TYPE_2;
-                Toast.makeText(context,"333333",Toast.LENGTH_LONG).show();
+                Toast.makeText(context,"3",Toast.LENGTH_LONG).show();
                 lessonTypes();
                 break;
         }
