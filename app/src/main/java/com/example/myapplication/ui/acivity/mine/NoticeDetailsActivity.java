268ef4ffce0e514665d19f0028bfda10ed86ccb8
XiaoHuisir <860663594@qq.com>
2020-01-07 12:45:00 +0800

    时间的转换new

--- app/src/main/java/com/example/myapplication/ui/acivity/mine/NoticeDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/ui/acivity/mine/NoticeDetailsActivity.java
@@ -44,8 +44,10 @@ public class NoticeDetailsActivity extends BaseActivity {
         int add_time = getIntent().getIntExtra("add_time", 0);
         txtNotName.setText("发布人：" + titles);
         txtNotData.setText(contents);
-//        String s = DateUtil.subStandardTime(String.valueOf(add_time));
-        txtNotTiem.setText(String.valueOf(add_time));
+        String s = DateUtil.subStandardTime(String.valueOf(add_time));
+        Long l = new Long((long)add_time);
+        String s1 = DateUtil.formatTime2String(l);
+        txtNotTiem.setText("发布时间：2019 "+s1);
     }
 
     @Override
