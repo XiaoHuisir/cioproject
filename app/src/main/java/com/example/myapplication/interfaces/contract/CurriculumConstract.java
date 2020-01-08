d3febb9cf5496d5e03c7b32607bfcb1b8cd6e3bf
zq134 <zq13408401721@139.com>
2020-01-09 02:34:20 +0800

    pdf下载

--- app/src/main/java/com/example/myapplication/interfaces/contract/CurriculumConstract.java
+++ app/src/main/java/com/example/myapplication/interfaces/contract/CurriculumConstract.java
@@ -1,17 +1,24 @@
 package com.example.myapplication.interfaces.contract;
 
 import com.example.myapplication.bean.CurriculumBean;
+import com.example.myapplication.bean.DownFileBean;
 import com.example.myapplication.interfaces.IBasePresenter;
 import com.example.myapplication.interfaces.IBaseView;
+
+import java.util.Map;
 
 public interface CurriculumConstract {
 
     interface View extends IBaseView{
         void getCurriculumReturn(CurriculumBean bean);
+
+        void downFileReturn(DownFileBean result);
     }
 
     interface Presenter extends IBasePresenter<View>{
         void getCurriculum(String curriculum_id);
+
+        void downFile(Map<String,String> map);
     }
 
 }
